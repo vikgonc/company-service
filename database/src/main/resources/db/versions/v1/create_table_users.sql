c11acb3aca094480f0ac8dad120a6b03cc3deb9a
vgoncharov <vgoncharov@zuzex.com>
2022-08-19 09:08:50 +0300

    Created liquibase migrations

--- database/src/main/resources/db/versions/v1/create_table_users.sql
+++ database/src/main/resources/db/versions/v1/create_table_users.sql
@@ -1,10 +1,11 @@
 --liquibase formatted sql
 
---changeset vikgonc:create_table_users
+--changeset vikgonc:1
 create table users
 (
     username varchar(255) not null primary key,
     password varchar(255) not null,
+    email    varchar(255) not null,
     role     varchar(255) not null
 );
 --rollback drop table users;

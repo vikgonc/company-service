5b90ff8a2c215362b9a5f8a107f8ee67488bb260
vgoncharov <vgoncharov@zuzex.com>
2022-09-06 07:48:49 +0300

    Returned netty server instead of tomcat,remover deprecated EmitterProcessor and replaced by Sinks.Many

--- car-showroom/src/main/java/com/zuzex/carshowroom/service/Impl/SocketCarDtoServiceImpl.java
+++ car-showroom/src/main/java/com/zuzex/carshowroom/service/Impl/SocketCarDtoServiceImpl.java
@@ -3,21 +3,21 @@ package com.zuzex.carshowroom.service.Impl;
 import com.zuzex.carshowroom.dto.CarDto;
 import com.zuzex.carshowroom.service.SocketCarDtoService;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.EmitterProcessor;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
 
 @Service
 public class SocketCarDtoServiceImpl implements SocketCarDtoService {
 
-    private final EmitterProcessor<CarDto> processor = EmitterProcessor.create();
+    private final Sinks.Many<CarDto> processor = Sinks.many().multicast().directAllOrNothing();
 
     @Override
     public void onNext(CarDto next) {
-        processor.onNext(next);
+        processor.tryEmitNext(next);
     }
 
     @Override
     public Flux<CarDto> getMessages() {
-        return processor.publish().autoConnect();
+        return processor.asFlux();
     }
 }

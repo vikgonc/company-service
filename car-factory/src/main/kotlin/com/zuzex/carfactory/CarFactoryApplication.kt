6516b2e81d79e413e4b4405c35f66f919bb63049
vgoncharov <vgoncharov@zuzex.com>
2022-09-12 14:10:11 +0300

    Created empty kotlin car-factory module

---
+++ car-factory/src/main/kotlin/com/zuzex/carfactory/CarFactoryApplication.kt
@@ -0,0 +1,11 @@
+package com.zuzex.carfactory
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarFactoryApplication
+
+fun main(args: Array<String>) {
+    runApplication<CarFactoryApplication>(*args)
+}
